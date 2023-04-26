f4b8de0b00c6041817e988e8bb957f9353502d5c
Massab Iftikhar <49832527+HaXer420@users.noreply.github.com>
2023-04-26 22:47:37 +0500

    Update apiCaller.js

--- src/api/apiCaller.js
+++ src/api/apiCaller.js
@@ -3,7 +3,7 @@ import configureAppStore from "../redux/store";
 
 import { accessToken } from "../redux/userDataSlice";
 
-const BASE_URL = "https://hmmxg26mva.us-east-1.awsapprunner.com/api/v1/";
+const BASE_URL = "https://nova.herokuapp.com/api/v1/";
 export const Method = {
   GET: "GET",
   POST: "POST",
